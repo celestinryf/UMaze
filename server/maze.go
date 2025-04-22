797f2a4b5af2d3dc059309eac7b3a847d2214367
Matthew Franchini <matchewf11@gmail.com>
2025-04-22 15:22:19 -0700

    I added logic to the maze class, still need to implement maze generation method

--- server/maze.go
+++ server/maze.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/rand"
+
 // The maze has a 2d array of rooms
 // The start is initially the start of the maze, then as
 // the user move, the start is what is updated.
@@ -25,9 +27,49 @@ func initMaze(rows, cols int) *maze {
 		}
 	}
 
-	// now we need to make the maze valid
-	// init the start
-	// init the end
+	newMaze.setUpOutsideWalls(rows, cols)
+	newMaze.createValidMaze()
+
+	sx, sy, ex, ey := selectStartEnd(rows, cols)
+	newMaze.start = newMaze.arr[sx][sy]
+	newMaze.end = newMaze.arr[ex][ey]
 
 	return newMaze
+}
+
+// Generates values for our start and end room.
+func selectStartEnd(rows, cols int) (int, int, int, int) {
+	startX := rand.Intn(rows)
+	startY := rand.Intn(cols)
+	var endX int
+	var endY int
+
+	for {
+		endX = rand.Intn(rows)
+		endY = rand.Intn(cols)
+		if startX != endX || startY != endY {
+			break
+		}
+	}
+	return startX, startY, endX, endY
+}
+
+// Sets all outside walls to false on the outside
+func (m *maze) setUpOutsideWalls(rows, cols int) {
+	// sets top and bottom walls
+	for i := range cols {
+		m.arr[0][i].up = false
+		m.arr[rows-1][i].down = false
+	}
+	// sets left and right walls
+	for i := range rows {
+		m.arr[i][0].left = false
+		m.arr[i][cols-1].right = false
+	}
+}
+
+// Create valid maze using a maze generation algorithm.
+// Assumes that the maze is newly initialized.
+func (m *maze) createValidMaze() {
+	// recursively makes maze
 }
