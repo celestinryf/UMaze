842cf79a8d61be8621824c80befbe94e1de4540e
Matthew Franchini <matchewf11@gmail.com>
2025-04-30 12:09:38 -0700

    Dummy Backend Server

--- server/main.go
+++ server/main.go
@@ -1,29 +1,77 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/rs/cors"
 )
 
+// this is temporary!!!
+// delete this later
+type dummyMaze struct {
+	grid [][]int
+	hero int
+}
+
+var currMaze dummyMaze
+
+// handles /api/game (gives me a hero and returns a new game instance)
+func gameHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var theHero int
+
+	err := json.NewDecoder(r.Body).Decode(&theHero)
+	if err != nil {
+		fmt.Println("Could not unmarshall json:", err)
+	}
+
+	currMaze = dummyMaze{
+		grid: [][]int{
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
+		},
+		hero: theHero,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(currMaze)
+}
+
+// handles /api/maze
+// put give user input, get gets curr game state (gives an int and minuses of user health)
+func mazeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "PUT" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var damage int
+
+	err := json.NewDecoder(r.Body).Decode(&damage)
+	if err != nil {
+		fmt.Println("Could not parse damage json:", err)
+	}
+
+	currMaze.hero -= damage
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(currMaze)
+}
+
+// Starting point of the server
 func main() {
-	mux := http.NewServeMux()
+	fmt.Println("Starting server on :8080")
 
-	mux.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(w, `{"message": "Hello from Go!"}`)
-	})
+	http.HandleFunc("/api/game", gameHandler)
+	http.HandleFunc("/api/maze", mazeHandler)
 
-	// Setup CORS options
-	handler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173"}, // Frontend origin
-		AllowCredentials: true,
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type"},
-	}).Handler(mux)
-
-	fmt.Println("Server running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
