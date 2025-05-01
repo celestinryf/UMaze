029922161d1b05f38732487bf5b26be5b4ec1c03
Matthew Franchini <matchewf11@gmail.com>
2025-05-01 12:42:15 -0700

    This is adding the /api/game POST implementation

--- server/main.go
+++ server/main.go
@@ -6,69 +6,52 @@ import (
 	"net/http"
 )
 
-// this is temporary!!!
-// delete this later
-type dummyMaze struct {
-	grid [][]int
-	hero int
+// this will be the serialized version of a new game
+type serialGame struct {
+	game string `json:"game"`
 }
 
-var currMaze dummyMaze
+type dummyGame struct {
+	dummy string
+}
 
-// handles /api/game (gives me a hero and returns a new game instance)
 func gameHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+
+	switch r.Method {
+	case "POST":
+		fmt.Println("Post Called: Call this when we want to start up a new/existing game")
+
+		var sg serialGame
+
+		err := json.NewDecoder(r.Body).Decode(&sg)
+		if err != nil {
+			fmt.Println("Could not unmarshall json:", err)
+		}
+
+		if sg.game == "" {
+			fmt.Println("We are creating and returning a new game")
+			newGame := dummyGame{
+				dummy: "This is just a test",
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(newGame)
+		} else {
+			fmt.Println("This is where we load up an old game, not implemented")
+		}
+	case "GET":
+		fmt.Println("Get Called: gives the current game status")
+	case "PUT":
+		fmt.Println("Gets called when the front wants to change the hero of the game")
+	default:
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
 	}
-
-	var theHero int
-
-	err := json.NewDecoder(r.Body).Decode(&theHero)
-	if err != nil {
-		fmt.Println("Could not unmarshall json:", err)
-	}
-
-	currMaze = dummyMaze{
-		grid: [][]int{
-			{0, 0, 0},
-			{0, 0, 0},
-			{0, 0, 0},
-		},
-		hero: theHero,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(currMaze)
-}
-
-// handles /api/maze
-// put give user input, get gets curr game state (gives an int and minuses of user health)
-func mazeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "PUT" {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var damage int
-
-	err := json.NewDecoder(r.Body).Decode(&damage)
-	if err != nil {
-		fmt.Println("Could not parse damage json:", err)
-	}
-
-	currMaze.hero -= damage
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(currMaze)
 }
 
 // Starting point of the server
 func main() {
 	fmt.Println("Starting server on :8080")
 
-	http.HandleFunc("/api/game", gameHandler)
-	http.HandleFunc("/api/maze", mazeHandler)
+	http.HandleFunc("/api/game", gameHandler) // sets up the game (generates a maze)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
