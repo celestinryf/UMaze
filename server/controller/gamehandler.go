0675f1f39723b2e1abb06a20d1be15ddf34efd0c
Matthew Franchini <matchewf11@gmail.com>
2025-05-02 11:09:54 -0700

    Bug fix in game handler

--- server/controller/gamehandler.go
+++ server/controller/gamehandler.go
@@ -7,20 +7,25 @@ import (
 	"github.com/celestinryf/go-backend/model"
 )
 
+type heroRequest struct {
+	HeroType int `json:"heroType"`
+}
+
 func GameHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
 		model.InitGame()
 	case "GET":
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(model.GetCurrGame)
+		json.NewEncoder(w).Encode(model.GetCurrGame())
 	case "PUT":
-		var heroType int
-		err := json.NewDecoder(r.Body).Decode(&heroType)
+		var heroReq heroRequest
+		err := json.NewDecoder(r.Body).Decode(&heroReq)
 		if err != nil {
 			http.Error(w, "Couldn't parse json", http.StatusBadRequest)
+			return
 		}
-		model.GetCurrGame().SetHero(heroType)
+		model.GetCurrGame().SetHero(heroReq.HeroType)
 	default:
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
