a591cdb2fd0d05545a17ec923f8d40001783985e
Matthew Franchini <matchewf11@gmail.com>
2025-05-11 22:19:49 -0700

    Sending the json

--- server/main.go
+++ server/main.go
@@ -10,7 +10,7 @@ import (
 // Starting point of the server side of the program
 func main() {
 	fmt.Println("Starting server on :8080")
-	http.HandleFunc("/api/game", controller.GameHandler)
+	http.HandleFunc("/api/game/", controller.GameHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
--- server/model/mazegen.go
+++ server/model/mazegen.go
@@ -1,15 +1,31 @@
 package model
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
-// Makes a new Grid
-func newGrid() [mazeSize][mazeSize]*room {
+// Room struct and initRoom must be defined elsewhere
+
+// newGrid creates a grid of Rooms from the generated boolean maze
+func newGrid() [mazeSize][mazeSize]*Room {
 	var modelGrid [mazeSize][mazeSize]bool
-	setupNewGrid(1, 1, modelGrid) // can be random
-	var grid [mazeSize][mazeSize]*room
+
+	// Fill with walls (false is wall)
 	for i := range modelGrid {
-		for j := range modelGrid[0] {
-			if modelGrid[i][j] == true {
+		for j := range modelGrid[i] {
+			modelGrid[i][j] = false
+		}
+	}
+
+	// Start maze generation
+	setupNewGrid(1, 1, &modelGrid)
+
+	// Convert boolean grid to Room grid
+	var grid [mazeSize][mazeSize]*Room
+	for i := range modelGrid {
+		for j := range modelGrid[i] {
+			if modelGrid[i][j] {
 				grid[i][j] = initRoom(path)
 			} else {
 				grid[i][j] = initRoom(wall)
@@ -19,23 +35,30 @@ func newGrid() [mazeSize][mazeSize]*room {
 	return grid
 }
 
-// Recursively generages a maze with true and false values
-func setupNewGrid(x, y int, grid [mazeSize][mazeSize]bool) {
+// setupNewGrid recursively carves out paths in the maze
+func setupNewGrid(x, y int, grid *[mazeSize][mazeSize]bool) {
+	fmt.Printf("Visiting (%d, %d)\n", x, y)
+
+	// Define possible directions (in steps of 2 to leave walls)
 	directions := [][2]int{
-		{0, 2},
-		{2, 0},
-		{0, -2},
-		{-2, 0},
+		{0, 2},  // right
+		{2, 0},  // down
+		{0, -2}, // left
+		{-2, 0}, // up
 	}
 	rand.Shuffle(len(directions), func(i, j int) {
 		directions[i], directions[j] = directions[j], directions[i]
 	})
+
+	// Mark current cell as path
 	grid[y][x] = true
+
+	// Try all directions
 	for _, dir := range directions {
 		newX := x + dir[0]
 		newY := y + dir[1]
 		if newX > 0 && newX < mazeSize-1 && newY > 0 && newY < mazeSize-1 && !grid[newY][newX] {
-			grid[newY][newX] = true
+			// Knock down wall between current and target cell
 			grid[y+dir[1]/2][x+dir[0]/2] = true
 			setupNewGrid(newX, newY, grid)
 		}
