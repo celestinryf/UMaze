f3bbd262303023c5f63595124f2be4a8c879d954
Matthew Franchini <matchewf11@gmail.com>
2025-05-12 19:43:32 -0700

    Commented it out

--- server/main.go
+++ server/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/celestinryf/go-backend/controller"
@@ -13,30 +11,30 @@ import (
 // Starting point of the server side of the program
 func main() {
 
-	db, err := sql.Open("sqlite3", "./example.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	// db, err := sql.Open("sqlite3", "./example.db")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer db.Close()
 
-	// Example query
-	rows, err := db.Query("SELECT id, name, age FROM user")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
+	// // Example query
+	// rows, err := db.Query("SELECT id, name, age FROM user")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer rows.Close()
 
-	fmt.Println("Users:")
-	for rows.Next() {
-		var id int
-		var name string
-		var age int
-		err = rows.Scan(&id, &name, &age)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, name, age)
-	}
+	// fmt.Println("Users:")
+	// for rows.Next() {
+	// 	var id int
+	// 	var name string
+	// 	var age int
+	// 	err = rows.Scan(&id, &name, &age)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println(id, name, age)
+	// }
 
 	fmt.Println("Starting server on :8080")
 	http.HandleFunc("/api/game/", controller.GameHandler)
