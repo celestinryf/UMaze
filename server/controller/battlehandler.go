a68b46db52d6561c359485ed057d8f3d79f034bd
Matthew Franchini <matchewf11@gmail.com>
2025-05-26 12:08:46 -0700

    Finished api/battle

--- server/controller/battlehandler.go
+++ server/controller/battlehandler.go
@@ -1,49 +1,60 @@
 package controller
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/celestinryf/go-backend/model"
+)
 
 // Handles battles
 func (s *Server) BattleHandler(w http.ResponseWriter, r *http.Request) {
 
-	// have to attack (recieves damage as well)
-	// have to use potion
+	w.Header().Set("Content-Type", "application/json")
+	log.Printf("Recieved %s request to %s", r.Method, r.URL.Path)
+
+	switch r.Method {
+	case http.MethodPut:
+
+		var CurrAttack struct {
+			Attack int          `json:"Attack"`
+			Potion model.Potion `json:"Potion"`
+		}
+
+		if err := json.NewDecoder(r.Body).Decode(&CurrAttack); err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		RoomMonster := s.game.TheMaze.Grid[s.game.TheMaze.CurrCoords.X][s.game.TheMaze.CurrCoords.X].RoomMonster
+		CurrHero := s.game.TheHero
+
+		if CurrAttack.Potion != model.NoPotion {
+			CurrHero.CurrHealth += 100                            // health from potion
+			CurrHero.AquiredPotions = CurrHero.AquiredPotions[1:] // take off first element
+		}
+
+		RoomMonster.CurrHealth -= CurrHero.Attack
+		if RoomMonster.CurrHealth > 0 {
+			CurrHero.CurrHealth -= RoomMonster.Attack
+		}
+
+		var AttackResult struct {
+			RoomMonster model.Monster `json:"Monster"`
+			CurrHero    model.Hero    `json:"Hero"`
+		}
+
+		if err := json.NewEncoder(w).Encode(AttackResult); err != nil {
+			http.Error(w, "Failed to encode hero and monster", http.StatusInternalServerError)
+		}
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(map[string]string{
+			"status":  "error",
+			"message": "Method not allowed",
+		})
+	}
 
 }
-
-// func GameHandler(w http.ResponseWriter, r *http.Request) {
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	log.Printf("Received %s request to %s", r.Method, r.URL.Path)
-
-// 	switch r.Method {
-// 	case "POST": // makes a new game given the hero id
-// 		type HeroIdJson struct {
-// 			HeroId int `json:"hero_id"`
-// 		}
-// 		var inputHero HeroIdJson
-// 		if err := json.NewDecoder(r.Body).Decode(&inputHero); err != nil {
-// 			http.Error(w, "Invalid request body", http.StatusBadRequest)
-// 			return
-// 		}
-// 		db, err := sql.Open("sqlite3", "./db/360Game.db")
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		defer db.Close()
-
-// 		model.InitGame(inputHero.HeroId, db)
-// 		if err := json.NewEncoder(w).Encode(model.MyGame); err != nil {
-// 			http.Error(w, "Failed to encode game state", http.StatusInternalServerError)
-// 		}
-// 	case "GET": // gets curr game json
-// 		if err := json.NewEncoder(w).Encode(model.MyGame); err != nil {
-// 			http.Error(w, "Failed to encode game state", http.StatusInternalServerError)
-// 		}
-// 	default:
-// 		w.WriteHeader(http.StatusMethodNotAllowed)
-// 		json.NewEncoder(w).Encode(map[string]string{
-// 			"status":  "error",
-// 			"message": "Method not allowed",
-// 		})
-// 	}
-// }
