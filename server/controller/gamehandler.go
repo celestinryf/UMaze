68d121c9361b63d24c807153b65c06098c6b2617
Matthew Franchini <matchewf11@gmail.com>
2025-05-27 11:56:33 -0700

    Temp fix for testing

--- server/controller/gamehandler.go
+++ server/controller/gamehandler.go
@@ -37,6 +37,15 @@ func (s *Server) GameHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Failed to encode game state", http.StatusInternalServerError)
 		}
 	case "GET": // gets curr game json
+
+		db, err := sql.Open("sqlite3", "./db/360Game.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		s.Game = model.InitGame(4, db)
+
 		if err := json.NewEncoder(w).Encode(s.Game); err != nil {
 			http.Error(w, "Failed to encode game state", http.StatusInternalServerError)
 		}
